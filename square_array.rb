d638897692b91e682a394ee2645f940b9014e775
IS Choi <insil321@hotmail.com>
2018-09-28 14:52:00 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,7 @@
 def square_array(array)
   # your code here
-  basket = ["apple 1","apple 2","apple 3","apple 4","apple 5","apple 6","apple 7","apple 8","apple 9","apple 10"]
+  numbers= []
  
-# Step 1,2,3,4,5
 basket.each do |apple|
     puts "Taking out #{apple}"
 end
